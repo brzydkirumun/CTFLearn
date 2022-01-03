17c32085cc9d432e17cc93425960656fa8205961
brzydkirumun <86882994+brzydkirumun@users.noreply.github.com>
2022-01-04 00:55:40 +0100

    init

---
+++ Simple programming/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    char c;
+    int count, curr0, curr1;
+    count = curr0 = curr1 = 0;
+
+    if (argc != 2)
+        return EXIT_FAILURE;
+
+    FILE *f = fopen(argv[1], "r");
+
+    while ((c = getc(f)) != EOF) {
+        if (c == '\n') {
+            if ((curr0 % 3 == 0 && curr0 != 0) || (curr1 % 2 == 0 && curr1 != 0))
+                ++count;
+            curr0 = curr1 = 0;
+        }
+        else if (c == '1')
+            ++curr1;
+        else if (c == '0')
+            ++curr0;
+    }
+    fclose(f);
+
+    printf("Number of lines in %s satisfying the property: %d", argv[1], count);
+    return EXIT_SUCCESS;
+}
