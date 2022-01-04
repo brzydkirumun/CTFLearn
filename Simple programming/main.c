e47b142024ba6fa3ab530d9b150e600277275593
brzydkirumun <86882994+brzydkirumun@users.noreply.github.com>
2022-01-04 03:03:26 +0100

    comments

--- Simple programming/main.c
+++ Simple programming/main.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// run this program, pass it path to data, it will output result
+
 int main(int argc, char *argv[]) {
     char c;
     int count, curr0, curr1;
     count = curr0 = curr1 = 0;
 
+    // path not passed
     if (argc != 2)
         return EXIT_FAILURE;
 
     FILE *f = fopen(argv[1], "r");
 
+    // read file
     while ((c = getc(f)) != EOF) {
         if (c == '\n') {
+            // clear counters on new line
             if (curr0 % 3 == 0 || curr1 % 2 == 0)
                 ++count;
             curr0 = curr1 = 0;
         }
-        else if (c == '1')
+        else if (c == '1') // counting 1's
             ++curr1;
-        else if (c == '0')
+        else if (c == '0') // counting 0's
             ++curr0;
     }
 
